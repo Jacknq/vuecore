e598c2b448ea191cdd16a1a142b479c0ba2ac1db
Jack85 <bousekr@gmail.com>
2017-12-06 17:39:51 +0100

    refact

--- vuecore/src/Views/separatets.ts
+++ vuecore/src/Views/separatets.ts
@@ -1,9 +1,7 @@
 import { Component, Vue } from "../ext1";
 //let BootstrapVue = require('bootstrap-vue');
 import multiselect from "vue-multiselect";
-//var multiselect = require('vue-multiselect').default;
-// name:string = 'separatets'
-//  name:'test'
+
 @Component({ components: { multiselect }, name: "ts" })
 export default class extends Vue {
   // separatets extends Vue
