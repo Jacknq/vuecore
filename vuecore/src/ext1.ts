9f9dcbd7f18f35527ece4611c877d1e01df3e8d4
Jack85 <bousekr@gmail.com>
2018-01-03 10:48:15 +0100

    removing comments

--- vuecore/src/ext1.ts
+++ vuecore/src/ext1.ts
@@ -1,23 +1,21 @@
 import { Component, Inject, Model, Prop, Watch } from "vue-property-decorator";
 export { Component, Inject, Model, Prop, Watch } from "vue-property-decorator";
 //import Vue from 'vue'
-import store,  { storeData, State } from "./System/store";
+import store, { storeData, State } from "./System/store";
 declare var require: any;
 import VueRouter from "vue-router";
 //import axio, { AxiosRequestConfig, AxiosPromise } from "axios";
 import * as d from "../code/Backend/repo/t4/hubsflow";
-import  moment from "moment";
+import moment from "moment";
 export { d };
 import * as b from "./ext";
-import  { Store } from "vuex";
+import { Store } from "vuex";
 b.Vue.use(VueRouter);
 
 //extending default vue with some more stuff
-// 3. Declare augmentation for Vue
 
 export class Vue extends b.Vue {
   $v: any;
-
 
   get db() {
     return this.$storets.state.db;
@@ -27,13 +25,11 @@ export class Vue extends b.Vue {
   }
   get vars() {
     return this.$storets.state.vars;
-  } 
+  }
   get $storets() {
     return this.$store as Store<State>; //store;//
   }
- 
 
-  //@Store setvars = commit('varsset')
   public log(val: String) {
     console.log(val);
   }
@@ -45,15 +41,14 @@ export class Vue extends b.Vue {
     this.$storets.commit("setvars", vars);
   }
   setdb(db: d.SgnRCloud): void {
-   this.$storets.commit("setdb", db);
+    this.$storets.commit("setdb", db);
   }
 }
-//}
 
-//var v = new Vue();
 //EXAMPLE OF DATETIME formatting - FILTER
-Vue.filter("dtformat", (val) =>{
-  if (val != null && val != "")  return moment(val).format(store.state.vars.dateformat);
+Vue.filter("dtformat", val => {
+  if (val != null && val != "")
+    return moment(val).format(store.state.vars.dateformat);
   return "";
 });
 
